afd2ec19a3264ef8000aea9c43a2700f4f8dab9e
j0hnd03 <alejo@klan1.com>
2026-05-25 22:08:24 -0500

    Add Bootstrap 5 button_group and button_toolbar components
    - button_group: supports size (sm/lg), vertical layout, accessibility attributes
    - button_toolbar: combines button groups with spacing support
    - Fix size classes for vertical groups (Bootstrap 5 doesn't support sizes on vertical)

--- src/klan1/html/bootstrap/button_group.php
+++ src/klan1/html/bootstrap/button_group.php
@@ -11,20 +11,28 @@ namespace k1lib\html\bootstrap;
  * @author  Alejandro Trujillo J. (J0hnd03)
  * @link    https://github.com/klan1/k1.lib-bootstrap
  * @link    https://github.com/twbs/bootstrap/blob/v5.3.8/site/src/content/docs/components/button-group.mdx
- * @link    https://github.com/twbs/bootstrap/blob/v5.3.8/site/src/content/docs/components/buttons.mdx
  * @license Apache-2.0
  * @version BETA
  */
 class button_group extends \k1lib\html\div {
 
     /**
-     * @param string $size Size (sm, md, lg)
+     * @param string $size Size (sm, lg) - leave empty for default
      * @param bool $vertical Stack vertically
      * @param string $label Accessibility label
      */
-    public function __construct($size = 'md', $vertical = FALSE, $label = 'Button group') {
-        $size_class = $size !== 'md' ? "btn-group-{$size}" : 'btn-group';
-        $class = $vertical ? "btn-group-vertical {$size_class}" : $size_class;
+    public function __construct($size = '', $vertical = FALSE, $label = 'Button group') {
+        if ($vertical) {
+            $class = 'btn-group-vertical';
+        } else {
+            $class = 'btn-group';
+            if ($size === 'sm') {
+                $class .= ' btn-group-sm';
+            } elseif ($size === 'lg') {
+                $class .= ' btn-group-lg';
+            }
+        }
+
         parent::__construct($class, NULL);
         $this->set_attrib('role', 'group');
         $this->set_attrib('aria-label', $label);
@@ -34,19 +42,22 @@ class button_group extends \k1lib\html\div {
      * Add a button to the group
      *
      * @param \k1lib\html\tag $button Button to add
-     * @param int|null $index Position to insert at (null = append)
-     * @param bool $insert If true, shift existing buttons to make space
      * @return $this
      */
-    public function add_button($button, $index = NULL, $insert = FALSE) {
-        if ($index === NULL) {
+    public function add_button($button) {
+        $this->append_child($button);
+        return $this;
+    }
+
+    /**
+     * Add multiple buttons at once
+     *
+     * @param array $buttons Array of button objects
+     * @return $this
+     */
+    public function add_buttons(array $buttons) {
+        foreach ($buttons as $button) {
             $this->append_child($button);
-        } elseif ($insert) {
-            $childs = $this->get_childs();
-            array_splice($childs, $index, 0, [$button]);
-            $this->rebuild_childs($childs);
-        } else {
-            $this->replace_child_at($index, $button);
         }
         return $this;
     }
@@ -70,45 +81,37 @@ class button_group extends \k1lib\html\div {
     public function count() {
         return count($this->get_childs());
     }
+}
+
+/**
+ * Bootstrap 5 Button Toolbar component
+ *
+ * Combines groups of button groups into button toolbars.
+ */
+class button_toolbar extends \k1lib\html\div {
 
     /**
-     * Remove a button by index
+     * @param string $label Accessibility label
+     */
+    public function __construct($label = 'Button toolbar') {
+        parent::__construct('btn-toolbar', NULL);
+        $this->set_attrib('role', 'toolbar');
+        $this->set_attrib('aria-label', $label);
+    }
+
+    /**
+     * Add a button group to the toolbar
      *
-     * @param int $index
+     * @param \k1lib\html\bootstrap\button_group $group
+     * @param string $class Additional classes like 'me-2' for spacing
      * @return $this
      */
-    public function remove_button($index) {
-        $childs = $this->get_childs();
-        if (isset($childs[$index])) {
-            array_splice($childs, $index, 1);
-            $this->rebuild_childs($childs);
+    public function add_group(button_group $group, $class = '') {
+        $group_div = new \k1lib\html\div('btn-group ' . $class);
+        foreach ($group->get_childs() as $child) {
+            $group_div->append_child($child);
         }
+        $this->append_child($group_div);
         return $this;
-    }
-
-    /**
-     * Rebuild childs array from given array
-     *
-     * @param array $childs
-     */
-    private function rebuild_childs($childs) {
-        $this->remove_childs();
-        foreach ($childs as $child) {
-            $this->append_child($child);
-        }
-    }
-
-    /**
-     * Replace a button at specific index
-     *
-     * @param int $index
-     * @param \k1lib\html\tag $button
-     */
-    private function replace_child_at($index, $button) {
-        $childs = $this->get_childs();
-        if (isset($childs[$index])) {
-            $childs[$index] = $button;
-            $this->rebuild_childs($childs);
-        }
     }
 }
