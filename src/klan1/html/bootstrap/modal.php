3eb5b63239d3531b3a3b3e67e5ccb0941ec145d6
j0hnd03 <alejo@klan1.com>
2026-05-27 14:32:33 -0500

    Complete docblocks for modal.php
    - Full class and constants documentation
    - Property docblocks with types
    - Complete constructor options documentation
    - add_footer_item method documentation

--- src/klan1/html/bootstrap/modal.php
+++ src/klan1/html/bootstrap/modal.php
@@ -5,33 +5,106 @@ namespace k1lib\html\bootstrap;
 /**
  * Bootstrap 5 Modal component
  *
+ * A modal is a dialog box/popup window that displays on top of the current page.
+ * It includes a header with a close button, a body with customizable content,
+ * and an optional footer with action buttons. Supports sizes, scrollable content,
+ * centered positioning, static backdrop, and keyboardDismiss configuration.
+ *
  * @author  Alejandro Trujillo J. (J0hnd03)
  * @link    https://github.com/klan1/k1.lib-bootstrap
  * @link    https://github.com/twbs/bootstrap/blob/v5.3.8/site/src/content/docs/components/modal.mdx
  * @license Apache-2.0
+ * @version 1.0.0
  */
 class modal extends \k1lib\html\div {
 
+    /**
+     * Small modal width (max-width: 300px)
+     */
     const SIZE_SM = 'modal-sm';
+
+    /**
+     * Large modal width (max-width: 800px)
+     */
     const SIZE_LG = 'modal-lg';
+
+    /**
+     * Extra large modal width (max-width: 1140px)
+     */
     const SIZE_XL = 'modal-xl';
+
+    /**
+     * Fullscreen below small breakpoint
+     */
     const SIZE_FULLSMALL = 'modal-fullscreen-sm';
+
+    /**
+     * Fullscreen below medium breakpoint
+     */
     const SIZE_FULLMD = 'modal-fullscreen-md';
+
+    /**
+     * Fullscreen below large breakpoint
+     */
     const SIZE_FULLLG = 'modal-fullscreen-lg';
+
+    /**
+     * Fullscreen below extra large breakpoint
+     */
     const SIZE_FULLXL = 'modal-fullscreen-xl';
+
+    /**
+     * Always fullscreen
+     */
     const SIZE_FULL = 'modal-fullscreen';
 
+    /**
+     * Dialog container element
+     * @var \k1lib\html\div
+     */
     protected $dialog = NULL;
+
+    /**
+     * Content container element
+     * @var \k1lib\html\div
+     */
     protected $content = NULL;
+
+    /**
+     * Modal header element
+     * @var \k1lib\html\div
+     */
     protected $header = NULL;
+
+    /**
+     * Modal body element
+     * @var \k1lib\html\div
+     */
     protected $body = NULL;
+
+    /**
+     * Modal footer element
+     * @var \k1lib\html\div
+     */
     protected $footer = NULL;
+
+    /**
+     * Modal title element
+     * @var \k1lib\html\h1|null
+     */
     protected $title = NULL;
+
+    /**
+     * Close button element
+     * @var \k1lib\html\button|null
+     */
     protected $close_btn = NULL;
 
     /**
-     * @param string $title Modal title text
-     * @param string $content Modal body content
+     * Creates a new Modal instance
+     *
+     * @param string $title Modal title text displayed in the header
+     * @param string $content Modal body content (HTML supported)
      * @param array $options Configuration array with keys:
      *   - id (string|null): Modal ID, auto-generated if null
      *   - size (string|null): SIZE_SM, SIZE_LG, SIZE_XL, SIZE_FULLSMALL, SIZE_FULLMD, SIZE_FULLLG, SIZE_FULLXL, or SIZE_FULL
@@ -128,6 +201,15 @@ class modal extends \k1lib\html\div {
         $this->append_child($this->dialog);
     }
 
+    /**
+     * Adds an element or button to the modal footer
+     *
+     * @param \k1lib\html\tag|string $element Element or text to add to footer.
+     *        If a tag object, it will be appended directly. If a string,
+     *        a button with that text will be created.
+     * @param string $position Position to add the element ('last' only supported)
+     * @return $this For method chaining
+     */
     public function add_footer_item($element, $position = 'last') {
         if ($element instanceof \k1lib\html\tag) {
             $this->footer->append_child($element);
